8400781f0716c868147475ae79acc1d05e01890b
Fredrik <ique@3h.se>
2014-09-30 22:31:40 +0200

    Think this is the basic structure I want to go with

---
+++ readme.md
@@ -0,0 +1,15 @@
+What to be expected from an outgoing webhook from Slack.
+
+    token=WbfcgFz37ILxmQ7NqXQ6Mjcx
+    team_id=T0001
+    channel_id=C2147483705
+    channel_name=test
+    timestamp=1355517523.000005
+    user_id=U2147483697
+    user_name=Steve
+    text=googlebot: What is the air-speed velocity of an unladen swallow?
+    trigger_word=googlebot:
+
+What you can output to the response (I think):
+
+https://api.slack.com/methods/chat.postMessage
--- src/Main.hs
+++ src/Main.hs
@@ -1,1 +1,61 @@
-main = print "hello"
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Main where
+
+import Data.Aeson (ToJSON (..), object, (.=))
+
+import Web.Scotty
+
+import Control.Monad.Trans
+
+import Data.Text.Lazy (Text)
+import Data.Monoid (mconcat)
+
+data Command = Help
+
+instance Parsable Command where
+  parseParam _ = Right Help
+
+data SlackRequest = SlackRequest Command SlackData
+
+data SlackResponse = SuccessResponse Text
+                   | FailResponse
+
+instance ToJSON SlackResponse where
+  toJSON (SuccessResponse t) = object ["text" .= t]
+  toJSON FailResponse        = object ["text" .= failMsg]
+
+failMsg :: Text
+failMsg = "Sorry, I don't know that command. Try `@lumibot help`"
+
+
+data SlackData = SlackData
+  { token       :: Text
+  , teamId      :: Text
+  , channelId   :: Text
+  , channelName :: Text
+  , timestamp   :: Text
+  , userId      :: Text
+  , userName    :: Text
+  , triggerWord :: Text
+  }
+
+main = scotty 3000 $ do
+  post "/" $ do
+    token       <- param "token"
+    teamId      <- param "team_id"
+    channelId   <- param "channel_id"
+    channelName <- param "channel_name"
+    timestamp   <- param "timestamp"
+    userId      <- param "user_id"
+    userName    <- param "user_name"
+    triggerWord <- param "trigger_word"
+
+    command     <- param "text"
+
+    response <- liftIO . handler $ SlackRequest command SlackData {..}
+    json response
+
+handler :: SlackRequest -> IO SlackResponse
+handler (SlackRequest Help _) = return $ SuccessResponse "This is the help message"
